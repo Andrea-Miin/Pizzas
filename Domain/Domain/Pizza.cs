28affdb43fb73002f16770eea72cebf3d5fec8f4
saekkimiin <saekkimiin@gmail.com>
2018-11-05 23:07:49 +0100

    hecho domain y infrastructure

---
+++ Domain/Domain/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Pizza : EntityBase
+    {
+        public string Name { get; set; }
+        public virtual ICollection<Ingredient> Id_Ingredient { get; set; }
+        public virtual ICollection<Comment> Id_Comment { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Price { get; set; }
+    }
+}
